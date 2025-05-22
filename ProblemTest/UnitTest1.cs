f5bb387288b6eb1680fb9bb3f9447fc90dfb1e33
yakovsmonarch <frag.2010@yandex.ru>
2025-05-22 18:48:17 +0800

    Добавил проект тестов.

---
+++ ProblemTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace ProblemTest;
+
+using ConsoleProject;
+using ConsoleProject.Data;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void ConvertRomanTest()
+    {
+        Assert.Pass();
+    }
+}
