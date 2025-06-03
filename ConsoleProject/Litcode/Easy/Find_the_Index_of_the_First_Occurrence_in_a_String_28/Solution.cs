a22b405053a5327f9c6fb76886671790304399e4
yakovsmonarch <frag.2010@yandex.ru>
2025-06-03 12:58:35 +0800

    Прошел базовые тесты "Find_the_Index_of_the_First_Occurrence_in_a_String_28" для нового решения.

--- ConsoleProject/Litcode/Easy/Find_the_Index_of_the_First_Occurrence_in_a_String_28/Solution.cs
+++ ConsoleProject/Litcode/Easy/Find_the_Index_of_the_First_Occurrence_in_a_String_28/Solution.cs
@@ -1,9 +1,30 @@
+using System.Data;
+
 namespace ConsoleProject.Litcode.Easy.Find_the_Index_of_the_First_Occurrence_in_a_String_28;
 
 public class Solution
 {
     public int StrStr(string haystack, string needle)
     {
-        return haystack.IndexOf(needle);
+        int charNeedleId = needle[0];
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] != charNeedleId) continue;
+
+            bool ok = true;
+            for (int j = 0; j < needle.Length; j++)
+            {
+                if (haystack[j] != needle[j])
+                {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
