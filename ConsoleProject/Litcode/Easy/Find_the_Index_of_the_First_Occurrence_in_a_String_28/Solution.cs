a44b5c8400b79dd64f87bf6b1a50292f45ca7a02
yakovsmonarch <frag.2010@yandex.ru>
2025-06-03 12:16:05 +0800

    Заготовка тестов для "Find_the_Index_of_the_First_Occurrence_in_a_String_28".

---
+++ ConsoleProject/Litcode/Easy/Find_the_Index_of_the_First_Occurrence_in_a_String_28/Solution.cs
@@ -0,0 +1,9 @@
+namespace ConsoleProject.Litcode.Easy.Find_the_Index_of_the_First_Occurrence_in_a_String_28;
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ProblemTest/Litcode/Easy/Find_the_Index_of_the_First_Occurrence_in_a_String_28.cs
@@ -0,0 +1,16 @@
+namespace ProblemTest.Litcode.Easy;
+
+public class Find_the_Index_of_the_First_Occurrence_in_a_String_28
+{
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void MergeTwoSortedListsTest()
+    {
+        Assert.Pass();
+    }
+}
