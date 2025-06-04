f8aad7a0f1515ece77075ee39716acc64ac9ee73
yakovsmonarch <frag.2010@yandex.ru>
2025-06-04 14:35:06 +0800

    Заготовка тестов для "Search_Insert_Position_35".

---
+++ ConsoleProject/Litcode/Easy/Search_Insert_Position_35/Solution.cs
@@ -0,0 +1,8 @@
+namespace ConsoleProject.Litcode.Easy.Search_Insert_Position_35;
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target) {
+        throw new NotImplementedException();
+    }
+}
---
+++ ProblemTest/Litcode/Easy/Search_Insert_Position_35.cs
@@ -0,0 +1,32 @@
+namespace ProblemTest.Litcode.Easy;
+
+public class Search_Insert_Position_35
+{
+    
+
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void StrStrTest()
+    {
+        Assert.Pass();
+    }
+
+    class DataSet
+    {
+        public readonly int[] Nums;
+        public readonly int Num;
+        public readonly int ResultId;
+
+        public DataSet(int[] nums, int num, int resultId)
+        {
+            Nums = nums;
+            Num = num;
+            ResultId = resultId;
+        }
+    }
+}
