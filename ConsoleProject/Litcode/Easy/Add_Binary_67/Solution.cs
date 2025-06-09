267488e24b297576a0e56c1fee35895350593c83
yakovsmonarch <frag.2010@yandex.ru>
2025-06-09 15:44:48 +0800

    Прошел базовые тесты для "Add_Binary_67".

--- ConsoleProject/Litcode/Easy/Add_Binary_67/Solution.cs
+++ ConsoleProject/Litcode/Easy/Add_Binary_67/Solution.cs
@@ -1,14 +1,16 @@
+using System.Numerics;
+
 namespace ConsoleProject.Litcode.Easy.Add_Binary_67;
 
 public class Solution
 {
     public string AddBinary(string a, string b)
     {
-        UInt32
-            numA = Convert.ToUInt32(a, 2),
-            numB = Convert.ToUInt32(b, 2);
-        UInt32 result = numA + numB;
-        
-        return Convert.ToString(result, 2);
+        BigInteger
+            bigA = BigInteger.Parse("0" + a, System.Globalization.NumberStyles.AllowBinarySpecifier),
+            bigB = BigInteger.Parse("0" + b, System.Globalization.NumberStyles.AllowBinarySpecifier);
+        BigInteger res = bigA + bigB;
+
+        return res.ToString("B").TrimStart('0');
     }
 }
--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -1,3 +1,18 @@
-﻿using ConsoleProject;
+﻿
+using System.Numerics;
 
-Console.WriteLine("Hello litcode!");
+string a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+string b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+
+//string test = "1101010";
+
+Console.WriteLine(a.Length);
+Console.WriteLine(b.Length);
+
+BigInteger bigIntA = BigInteger.Parse(a);
+BigInteger bigIntB = BigInteger.Parse(b);
+//BigInteger bigTest = BigInteger.Parse("0" + test, System.Globalization.NumberStyles.AllowBinarySpecifier);
+BigInteger bigRes = bigIntA + bigIntB;
+
+string strRes = bigRes.ToString("B"); //bigIntBinary.ToString();
+Console.WriteLine(strRes);
